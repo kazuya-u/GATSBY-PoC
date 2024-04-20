65ee1489dad76d9f4510c2448212ede08338514d
Kazuya Umeki <kazuyaumeki1115@gmail.com>
2024-04-21 04:02:47 +0900

    Tutorial.

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='About Me'>
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
+  )
+}
+
+export const Head = () => <Seo title="About Me" />
+
+export default AboutPage
